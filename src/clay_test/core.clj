aaf11bcb41388361ddbbd809dc78b54c407aa15c
Timothy Pratley <timothypratley@gmail.com>
2025-09-08 22:24:52 -0700

    use-first-as-index flag turned true

--- src/clay_test/core.clj
+++ src/clay_test/core.clj
@@ -9,6 +9,7 @@
                                      "components/component3/docs/index.clj"
                                      "components/component1/docs/index.clj"
                                      "components/component2/docs/index.clj"]
+               :first-as-index      true
                :browse              false
                :live-reload         true
                :format              [:quarto :html]
